4729639785148d5c73fa98c37b09104e5061bd3e
FrancoMessina <franco.m2017@outlook.com>
2022-03-24 16:37:08 -0300

    Ejercicio I08 - Dibujando un triángulo rectángulo

---
+++ Primera Unidad/Ejercicio I08/Ejercicio I08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ejercicio_I08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int altura = 5;
+            
+            for (int i = 0; i < altura; i++)
+            {
+                MostrarAstericos(i * 2); //
+                Console.WriteLine();
+            }
+        }
+        public static void MostrarAstericos(int cantidadAstericos)
+        {
+            for(int i = 0; i <= cantidadAstericos; i++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
