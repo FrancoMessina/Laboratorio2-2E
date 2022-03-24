5bb216190fe719487cc3b98666a6472ee3236d33
FrancoMessina <franco.m2017@outlook.com>
2022-03-24 14:44:40 -0300

    Ejercicio 7 Guia 1

---
+++ Segunda Unidad/Ejercicio I07/Ejercicio I07/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using LogicaNegocio;
+using System.Collections.Generic;
+namespace Ejercicio_I07
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Empleado> listaEmpleados = new List<Empleado>();
+            int cantidadEmpleados;
+            bool esNumero;
+            Console.WriteLine("Ingresa la cantidad de empleados : ");
+            esNumero = int.TryParse(Console.ReadLine(), out cantidadEmpleados);
+            while (!esNumero)
+            {
+                Console.WriteLine("Error. Reingresa la cantidad de empleados : ");
+                esNumero = int.TryParse(Console.ReadLine(), out cantidadEmpleados);
+            }
+            for(int i = 0 ; i < cantidadEmpleados; i++)
+            {
+                Empleado empleado = new Empleado();
+                float valorHora;
+                int antiguedadAnios;
+                int cantidadHoras;
+                Console.WriteLine("Ingresa nombre");
+                empleado.Nombre = Console.ReadLine();
+                Console.WriteLine("Ingresar valor por hora");
+                float.TryParse(Console.ReadLine(), out valorHora);
+                empleado.ValorHora = valorHora;
+                Console.WriteLine("Ingresar cantidad horas");
+                int.TryParse(Console.ReadLine(), out cantidadHoras);
+                empleado.AntiguedadAnios = cantidadHoras;
+                Console.WriteLine("Ingresar antiguedad Años");
+                int.TryParse(Console.ReadLine(), out antiguedadAnios);
+                empleado.AntiguedadAnios = antiguedadAnios;
+                listaEmpleados.Add(empleado);
+            }
+            foreach (Empleado empleado in listaEmpleados)
+            {
+                empleado.MostrarInfoEmpleado();
+            }
+           
+        }
+    }
+}
---
+++ Segunda Unidad/Ejercicio I07/LogicaNegocio/Empleado.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public class Empleado
+    {
+        //Encapsulamiento
+        private float valorHora;
+        //private string nombre;
+        private int cantidadHoras;
+        private int antiguedadAnios;
+        //Props
+        public float ValorHora
+        {
+            get { return valorHora; }
+            set
+            {
+                if (value > 0)
+                    valorHora = value;
+                else
+                    valorHora = -1;
+            }
+        }
+        public string Nombre { get; set; }
+        public int CantidadHoras
+        {
+            set
+            {
+                if (value > 0)
+                    cantidadHoras = value;
+                else
+                    cantidadHoras = -1;
+            }
+        }
+        public int AntiguedadAnios
+        {
+            get { return antiguedadAnios; }
+            set
+            {
+                if (value > 0)
+                    antiguedadAnios = value;
+                else
+                    antiguedadAnios = -1;
+            }
+        }
+        public float CalcularImporteBruto()
+        {
+            float cantidadXHoras = valorHora * cantidadHoras;
+            float importeBruto;
+            importeBruto = cantidadXHoras + (antiguedadAnios * 150);
+            return importeBruto;
+        }
+        public float CalcularImporteNeto()
+        {
+            float descuento;
+            float importeBruto;
+            float importeNeto;
+            importeBruto = CalcularImporteBruto();
+            descuento = importeBruto * 13 / 100;
+            importeNeto = importeBruto - descuento;
+            return importeNeto;
+        }
+        /*
+         * Mostrar el recibo correspondiente con el nombre, la antigüedad, el valor hora,
+         * el total a cobrar en bruto y el total a cobrar neto de todos los empleados ingresados.
+
+
+         */
+        public void MostrarInfoEmpleado()//Esta funcion como contiene un CW deberia ir en vista no en Logica.
+        {
+            Console.WriteLine($"Nombre: {Nombre}\n" +
+                              $"Valor por hora : {valorHora} \n" +
+                              $"Antiguedad : {antiguedadAnios} \n" +
+                              $"Importe bruto : {CalcularImporteBruto()}\n" +
+                              $"Importe Neto : {CalcularImporteNeto()}");
+        }
+    }
+}
