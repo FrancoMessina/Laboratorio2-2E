359dc59e8f496d8ed775c97dcd551f946e6be043
FrancoMessina <franco.m2017@outlook.com>
2022-03-24 19:33:18 -0300

    Ejercicio 2 Unidad 2

---
+++ Segunda Unidad/EjercicioI02/EjercicioI02/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using LogicaNegocio;
+namespace EjercicioI02
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Consigna#
+                    Realizar un programa que sume números enteros hasta que el usuario lo determine por medio de un mensaje "¿Desea continuar? (S/N)".
+
+                    Crear la clase Validador y el método estático ValidarRespuesta, que validará el ingreso de respuestas.
+
+                    El método devolverá un valor de tipo booleano, TRUE si se ingresó una 'S' y FALSE si se ingresó cualquier otro valor.
+
+             */
+            int acumulador = 0;
+            int numeroIngresado;
+            bool esNumero;
+            string respuesta;
+            do
+            {
+                Console.WriteLine("Ingresa un numero : ");
+                esNumero = int.TryParse(Console.ReadLine(), out numeroIngresado);
+                while (!esNumero)
+                {
+                    Console.WriteLine("Error. Reingresar un numero valido : ");
+                    esNumero = int.TryParse(Console.ReadLine(), out numeroIngresado);
+                }
+                acumulador += numeroIngresado;
+                Console.WriteLine("¿Desea continuar? (S/N).");
+                respuesta = Console.ReadLine();
+            }while (!Validador.ValidarRespuesta(respuesta));
+            Console.WriteLine($"Total acumulado : {acumulador}");
+        }
+
+    }
+}
---
+++ Segunda Unidad/EjercicioI02/LogicaNegocio/Validador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio
+{
+    public class Validador
+    {
+        public static bool ValidarRespuesta(string respuesta)
+        {
+            if (respuesta == "S")
+                return true;
+            return false;
+        }
+    }
+}
