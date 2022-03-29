75df1867e1824ab39696ab5ec65e322debc282c9
FrancoMessina <franco.m2017@outlook.com>
2022-03-29 13:41:10 -0300

    Clase 3 POO

---
+++ Segunda Unidad/Ejercicio I04/LogicaNegocio/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicaNegocio
+{
+    public class Calculadora
+    {
+        //Calcular (público): Recibirá tres parámetros, el primer operando, el segundo operando
+        //y la operación matemática. El método devolverá el resultado de la operación.
+        public static float Calcular()
+
+    }
+}
---
+++ Segunda Unidad/Ejercicio I05/Ejercicio I05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Ejercicio_I05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Consigna#
+                Crear una aplicación de consola que permita al usuario ingresar un número entero.
+
+                Desarrollar un método estático que reciba un número y devuelva la tabla de multiplicación de
+                ese número en formato string.
+
+                Se deberá utilizar la clase StringBuilder combinada con strings interpolados para armar el
+                resultado.
+
+                Mostrar en la consola el resultado.
+
+                Por ejemplo, si se ingresa el número 2 la salida deberá ser:
+             */
+            Console.WriteLine("Ingresa numero  : ");
+            int.TryParse(Console.ReadLine(), out int numeroIngresado);
+            string tabla = DevolverTabla(numeroIngresado);
+            Console.WriteLine(tabla);
+        }
+        public static string DevolverTabla(int num) 
+        { 
+            StringBuilder sb = new StringBuilder();
+            for(int i = 1; i < 10; i++)
+            {
+                sb.Append($"{num} x {i} = {i * num}");
+                sb.AppendLine();
+            }
+            return sb.ToString();
+        }
+    }
+}
