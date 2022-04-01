b7c4dfbff691ef6ac3dcbe18855ac1bc0bb0f61b
FrancoMessina <franco.m2017@outlook.com>
2022-03-31 21:31:52 -0300

    ejercicio estanteria faltan los ultimos 2 puntos

---
+++ Cuarta Unidad/Ejercicio C02/La estantería/Entidades/Estante.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Estante
+    {
+        private int ubicacionEstante;
+        private Producto[] productos;
+
+        private Estante(int capacidad)
+        {
+            this.productos = new Producto[capacidad];
+            
+        }
+        public Estante(int capacidad, int ubicacionEstante) :this(capacidad)
+        {
+            this.ubicacionEstante = ubicacionEstante;
+        }
+        public Producto[] GetProductos()
+        {
+            return this.productos;
+        }
+        public static string MostrarEstante(Estante estante)
+        {
+            Producto[] productosEstante = estante.GetProductos();
+            StringBuilder mensajeEstantes = new StringBuilder();
+            mensajeEstantes.AppendLine($"Ubicacion Estante: {estante.ubicacionEstante}");
+            for (int i = 0; i < productosEstante.Length; i++)
+            {
+                if(!(productosEstante[i] is null))
+                {
+                    mensajeEstantes.AppendLine(Producto.MostrarProducto(productosEstante[i]));
+                }
+            }
+            return mensajeEstantes.ToString();
+        }
+        public static bool operator ==(Estante e, Producto p)
+        {
+            Producto[] productosEstante = e.GetProductos();
+            for (int i = 0; productosEstante.Length < 0; i++)
+            {
+                if (productosEstante[i] == p)
+                    return true;
+            }
+            return false;
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+            return !(e == p);
+        }
+    }
+}
---
+++ Cuarta Unidad/Ejercicio C02/La estantería/Entidades/Producto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+
+        public Producto(string marca, string codigo, float precio)
+        {
+            this.marca = marca;
+            this.precio = precio;
+            this.codigoDeBarra = codigo;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return $"Marca : {p.marca}, Codigo de Barra: {(string)p}, Precio: {p.precio}";
+        }
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        //== (Producto, Producto): Retornará true si las marcas y
+        //códigos de barra son iguales, false caso contrario.
+        public static bool operator == (Producto p1, Producto p2)
+        {
+            if (!(p1 is null || p2 is null))
+            {
+                return (p1.marca == p2.marca && p1.codigoDeBarra == p2.codigoDeBarra);
+            }
+            return false;
+        }
+        public static bool operator != (Producto p1, Producto p2)
+        {
+            return !(p1 == p2);
+        }
+        //== (Producto, string): Retornará true si la marca del producto coincide
+        //con la cadena pasada como argumento, false caso contrario.
+
+        public static bool operator ==(Producto p1, string marca)
+        {
+            return p1.marca == marca;
+        }
+        public static bool operator !=(Producto p1, string marca)
+        {
+            return !(p1.marca == marca);
+        }
+    }
+}
