490edcaebb9536fe54fc681c49264ee4573adec6
FrancoMessina <franco.m2017@outlook.com>
2022-04-03 22:11:46 -0300

    ejercicios unidad 3. A01, I04.

---
+++ Tercera Unidad/Ejercicio A01/EmpresaTransporte/Entidades/Conductor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+namespace Entidades
+{
+    public class Conductor
+    {
+        private string nombre;
+        private int[] kilometrosPorDia = new int[7];
+
+        public Conductor(string nombre)
+        {
+            SetNombre(nombre);
+            CargarKilometrosDefault();
+        }
+        public string GetNombre()
+        {
+            return this.nombre;
+        }
+        public int[] GetKilometrosPorDia()
+        {
+            return this.kilometrosPorDia;
+        }
+        public void SetNombre(string nombre)
+        {
+             this.nombre = nombre;
+        }
+        private void CargarKilometrosDefault()
+        {
+            for(int i = 0; i < this.kilometrosPorDia.Length; i++)
+            {
+                this.kilometrosPorDia[i] = 0;
+            }
+        }
+        public bool SetKilometroDia(int dia, int cantidadKilometros)
+        {
+            if(dia > 0 && dia < 8)
+            {
+                switch (dia)
+                {
+                    case 1:
+                        kilometrosPorDia[0] += cantidadKilometros;
+                        break;
+                    case 2:
+                        kilometrosPorDia[1] += cantidadKilometros;
+                        break;
+                    case 3:
+                        kilometrosPorDia[2] += cantidadKilometros;
+                        break;
+                    case 4:
+                        kilometrosPorDia[3] += cantidadKilometros;
+                        break;
+                    case 5:
+                        kilometrosPorDia[4] += cantidadKilometros;
+                        break;
+                    case 6:
+                        kilometrosPorDia[5] += cantidadKilometros;
+                        break;
+                    case 7:
+                        kilometrosPorDia[6] += cantidadKilometros;
+                        break;
+                }
+                return true;
+            }
+            return false;
+        }
+        public string MostrarConductor()
+        {
+            StringBuilder datosConductor = new StringBuilder();
+            int[] listaKilometros = GetKilometrosPorDia();
+            datosConductor.AppendLine($"Nombre : {this.GetNombre()}");
+            for (int i = 0; i < listaKilometros.Length; i++)
+            {
+                datosConductor.AppendLine($"Dia {i+1}: {listaKilometros[i]} kilometros");
+            }
+            return datosConductor.ToString();
+        }
+        
+    }
+}
---
+++ Tercera Unidad/Ejercicio A01/EmpresaTransporte/Vista/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Entidades;
+namespace Vista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia, kms;
+            string nombre;
+            Conductor[] conductores = new Conductor[3];
+            for(int i = 0; i < conductores.Length; i++)
+            {
+                Console.WriteLine("Ingresar nombre del conductor.");
+                nombre = Console.ReadLine();
+                conductores[i] = new Conductor(nombre);
+                for (int j = 0; j < 7; j++)
+                {
+                    dia = j + 1;
+                    Console.WriteLine($"Ingresar kms del dia : {j + 1}");
+                    int.TryParse(Console.ReadLine(), out kms);
+                    conductores[i].SetKilometroDia(dia, kms);
+                }
+            }
+            for(int i = 0; i < conductores.Length; i++)
+            {
+                Console.WriteLine(conductores[i].MostrarConductor());
+            }
+            Console.ReadKey();
+        }
+    }
+}
